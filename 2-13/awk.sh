3815c09c67d50ccb6b36051efdb6719ebf6347df
xunmengdeganjue <542873805@qq.com>
2015-06-10 09:16:53 -0700

    dfdf

--- 2-13/awk.sh
+++ 2-13/awk.sh
@@ -1,8 +1,7 @@
-#! /usr/bin
-awk -f 
-BEGIN{
-	printf("#include \"../include/apue.h\"")
-	printf("#include <errno.h> \n")
+#! /bin/bash
+awk 'BEGIN {
+	printf("#include \"../include/apue.h\"\n")
+	printf("#include <errno.h>\n")
 	printf("#include <limits.h> \n")
 	printf("\n")
 	printf("static void pr_sysconf(char *,int);\n")
@@ -15,7 +14,7 @@ BEGIN{
 	FS="\t+"
 	while (getline < "sysconf.sym" > 0){
 		printf("#ifdef %s\n",$1)
-		printf("\tprintf(\"%s defined to be %%ld\\n\")\n",(long)%s+0);\n",$1,$1)
+		printf("\tprintf(\"%s defined to be %%ld\\n\",(long)%s+0);\n",$1,$1)
 		printf("#else\n")
 		printf("\tprintf(\"no symbol for %s\\n\");\n",$1)
 		printf("#endif\n")
@@ -28,7 +27,7 @@ BEGIN{
 	close("sysconf.sym")
 	while (getline < "pathconf.sym" > 0){
 		printf("#ifdef %s\n",$1)
-		printf("\tprintf(\"%s defined to be %%ld\\n\")\n",(long)%s+0);\n,$1,$1)
+		printf("\tprintf(\"%s defined to be %%ld\\n\",(long)%s+0);\n",$1,$1)
 		printf("#else\n")
 		printf("\tprintf(\"no symbol for %s\\n\")\n",$1)
 		printf("#endif\n")
@@ -40,7 +39,7 @@ BEGIN{
 	close("pathconf.sym")
 	exit
 }
-END{
+END {
 	printf("\texit(0);\n")
 	printf("}\n\n")
 	printf("static void pr_sysconf(char *mesg,int name)\n")
@@ -48,38 +47,35 @@ END{
 	printf("\tlong val;\n\n")
 	printf("\tfputs(mesg,stdout);\n")
 	printf("\terrno = 0;\n")
-	printf("\tif ((var = sysconf(name))<0)") {\n")
+	printf("\tif ((var = sysconf(name))<0) {\n")
 	printf("\t\tif (errno != 0){\n")
 	printf("\t\t\tif(errno == EINVAL)\n")
 	printf("\t\t\t\tfputs(\"(not supported)\\n\",stdout);\n")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	printf("")
-	
+	printf("\t\t\telse\n")
+	printf("\t\t\t\terr_sys(\"sysconf error\");\n")
+	printf("\t\telse{\n")
+	printf("\t\t\tfputs(\"\(no limit)\\n\",stdout);\n")
+	printf("\t\t}\n")
+	printf("\t}else{\n")
+	printf("\t\tprintf(\"%%ld\\n\",val);\n")
+	printf("\t}\n")
+	printf("}\n\n")
+	printf("static void\n")
+	printf("pr_pathconf(char *mesg,char *path, int name)\n")
+	printf("{\n")
+	printf("\tlong val;\n")
+	printf("\n")
+	printf("\tfputs(mesg,stdout);\n")
+	printf("\terrno = 0;\n")
+	printf("\tif ((val = pathconf(path, name)) < 0){\n")
+	printf("\t\tif (errno != 0){\n")
+	printf("\t\t\tif(errno == EINVAL)\n")
+	printf("\t\t\t\tfputs(\"(not supported)\\n\",stdout);\n")
+	printf("\t\t\telse\n")
+	printf("\t\t\t\terr_sys(\"pathconf error,path = %%s\",path);\n")
+	printf("\t\t}\n")
+	printf("\telse{\n")
+	printf("\t\tprintf(\"%%ld\\n\");\n")
+	printf("\t}\n")
 	printf("}\n")
-}
-
-
-
+}'
