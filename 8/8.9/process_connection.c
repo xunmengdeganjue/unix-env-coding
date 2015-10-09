8d2aee2ed9a2f46546cbe59076249a047c3d217d
xunmengdeganjue <542873805@qq.com>
2015-10-09 11:25:57 +0800

    learn the 8.9 then write the test code

---
+++ 8/8.9/process_connection.c
@@ -0,0 +1,24 @@
+/*
+ *------------------------------------------------------------------------------
+ *
+ *    Copyright (C) 2015-2016 Chengdu Skysoft Info&Tech Co., Ltd. All rights reserved.
+ *    All source codes is distributed under the U-License.
+ *
+ *    CPU:        x86  
+ *    RTOS:      
+ *    Version:    0.50.00
+ *    Released by Chengdu Skysoft Info&Tech Co., Ltd.
+ *                  2015/10/09.
+ *
+ *
+ *  File Name    : process_connection.c
+ *  Create Date  : 2015/09/20
+ *  Author   : Nick Li
+ *  Description  : This progrom is just to test the connection between the processes.
+ *-------------------------------------------------------------------------------
+ */
+ 
+#include"../../include/apue.h"
+//#include<sys/wait.h>
+
+
