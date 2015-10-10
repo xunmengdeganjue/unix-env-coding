f082a19e23e6280fc87d71c1404dd8b571036aa7
xunmengdeganjue <542873805@qq.com>
2015-10-10 16:06:19 +0800

    update the code

--- 10/10.3/signal.c
+++ 10/10.3/signal.c
@@ -1,3 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<signal.h>
 
+static void sig_usr(int); /*one handler for both signals*/
+int main(void)
+{
+	if(signal(SIGUSR1,sig_usr) == SIG_ERR){
+		printf("can't catch SIGUSR1\n");
+	}
+	if(signal(SIGUSR2,sig_usr) == SIG_ERR){
+		printf("can't catch SIGUSR2\n");
+	}
+	for(;;){
+		pause();//let the program 
+	}
+}
+static void sig_usr(int signo){/*argument is signal number*/
+	if (signo == SIGUSR1){
+		printf("received SIGUSR1 [%d]\n",signo);
+	}else if (signo == SIGUSR2){
+		printf("received SIGUSR2 [%d]\n",signo);
+	}else{
+		printf("received signal %d \n",signo);
+	}
+}
