ed783c8a975aaca8178ed28fa708f20c8c0821ba
xunmengdeganjue <542873805@qq.com>
2015-10-11 08:34:06 -0700

    add the 10.6 folder

---
+++ 10/10.6/loginagain.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<pwd.h>
+#include<signal.h>
+
+static void my_alarm(int signo){
+	struct passwd *rootptr;
+	
+	printf("in signal handler\n");
+	if((rootptr = getpwnam("root")) == NULL){
+		printf("getpwname(root) error!\n");
+	alarm(1);
+	}
+}
+int main(void){
+	struct passwd *ptr;
+	signal(SIGALRM,my_alarm);
+	alarm(1);
+	for(;;){
+		if((ptr = getpwnam("sar") )== NULL){
+			printf("getpwname error");
+		}
+		if(strcmp(ptr->pw_name,"sar") != 0){
+			printf("return value corrupted!,pw_name = %s\n",ptr->pw_name);
+		}
+	}
+}
