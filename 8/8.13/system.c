525aa060d2f5c0be332057c30ddeb4338de915fa
xunmengdeganjue <542873805@qq.com>
2015-10-18 07:59:50 -0700

    update

--- 8/8.13/system.c
+++ 8/8.13/system.c
@@ -1,6 +1,25 @@
 #include<stdio.h>
 #include<sys/wait.h>
 #include<unistd.h>
+#include<stdlib.h>
+#include<errno.h> /*for EINTR errno*/
+
+void pr_exit(int status)
+{
+	if(WIFEXITED(status)){
+		printf("normal termination,exit status=%d\n",WEXITSTATUS(status));
+	}else if (WIFSIGNALED(status)){
+		printf("abnomal termination,signal number=%d%s\n",WTERMSIG(status),
+#ifdef WCOREDUMP
+		WCOREDUMP(status)?"(core file generated)":"");
+#else 
+	"" );
+#endif
+	}else if(WIFSTOPPED(status)){
+		printf("chiled stopped,signal number=%d\n",WSTOPSIG(status));
+	}
+
+}
 
 int system(const char *cmdstring)
 {
@@ -11,7 +30,7 @@ int system(const char *cmdstring)
 	}
 	if((pid = fork())<0){
 		status  =-1;
-	}else if(pif ==0){/*child*/
+	}else if(pid ==0){/*child*/
 		execl("/bin/sh","sh","-c",cmdstring,(char *)0);
 		_exit(127); /*execl error*/
 	}else{
@@ -32,7 +51,7 @@ int main(void)
 	}
 	pr_exit(status);
 	
-	if((status = system(nosuchcommand))<0){
+	if((status = system("nosuchcommand"))<0){
 		printf("system() error!\n");
 	}
 	pr_exit(status);
